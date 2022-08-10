3d30b3f429e14bab0ab3441c1e6fee1be09f157e
Nika Stankevich <kladnickaya@gmail.com>
2022-08-10 23:26:40 +0300

    feat solver task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let sortedArr = [];
+
+  if (matrix) {
+    matrix.forEach((el, i) => {
+      if (i%2) {
+        sortedArr = sortedArr.concat(el.reverse()); 
+      } else {
+        sortedArr = sortedArr.concat(el); 
+      }
+    });
+  }
+
+  return sortedArr;
 }
